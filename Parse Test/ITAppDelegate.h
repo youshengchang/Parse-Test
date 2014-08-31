2a6a160f1468003a352162be9f629841236ecb60
youshengchang <changyousheng@yahoo.com>
2014-08-30 21:47:24 -0700

    Initial Commit

---
+++ Parse Test/ITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITAppDelegate.h
+//  Parse Test
+//
+//  Created by yousheng chang on 8/30/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Parse Test/ITViewController.m
@@ -0,0 +1,29 @@
+//
+//  ITViewController.m
+//  Parse Test
+//
+//  Created by yousheng chang on 8/30/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import "ITViewController.h"
+
+@interface ITViewController ()
+
+@end
+
+@implementation ITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
